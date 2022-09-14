5f54855f1cb0b7e0d0ed028193ac25d425c6ce70
r-e-d-ant <mugishathierry65@gmail.com>
2022-09-14 13:37:09 +0200

    added basic routes

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Views
+def home(request):
+	HttpResponse('<h1>Hello, World! This is a home page<h1/>')
