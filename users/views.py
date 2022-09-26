cbea2d51864451b0e68403d0efd4365c17f9c97a
r-e-d-ant <mugishathierry65@gmail.com>
2022-09-26 09:28:41 +0200

    fixed bug

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            username = username.cleaned_data.get('username')
+            username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
