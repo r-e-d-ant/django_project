921c1b2e802b7b6a4537c7eda6eb9819f1fddd26
r-e-d-ant <mugishathierry65@gmail.com>
2022-09-26 09:06:47 +0200

    settled user reg view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+# The views here.
+def register(request):
+    # instance of the form
+    form = UserCreationForm()
+    return render(request, 'users/register.html', { 'form' : form })
